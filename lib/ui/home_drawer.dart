62c4c015dd5351e305079655f9b8861f5dac32eb
luofeng <1915327117@qq.com>
2018-03-28 18:23:14 +0800

    add header

--- lib/ui/home_drawer.dart
+++ lib/ui/home_drawer.dart
@@ -22,19 +22,44 @@ class DrawerHeaderState extends State<DrawerHeader> {
   @override
   Widget build(BuildContext context) {
     return new Container(
-      padding: new EdgeInsets.all(16.0),
-      child: new Row(
-        children: <Widget>[
-          new Align(
-            alignment: Alignment.centerLeft,
-            child: new Text('Hello'),
-          ),
-          new Align(
-            alignment: Alignment.centerRight,
-            child: new Text("Hello"),
-          )
-        ],
-      ),
+        color: Colors.red,
+        height: 180.0,
+        child: new Stack(
+          children: <Widget>[
+            new Positioned(
+              left: 16.0,
+              top: 16.0,
+              child: new Container(
+                width: 64.0,
+                height: 64.0,
+                child: new CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: new NetworkImage(
+                      'https://avatars2.githubusercontent.com/u/14973323?s=460&v=4'),
+                ),
+              ),
+            ),
+            new Positioned(
+                bottom: 16.0,
+                left: 16.0,
+                child: new Container(
+                    height: 40.0,
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Text(
+                            'JuZiSang',
+                            style: new TextStyle(
+                                color: Colors.white, fontSize: 18.0)),
+                        new Text('积分：0', style: new TextStyle(
+                            color: Colors.white70, fontSize: 14.0),
+                        )
+                      ],
+                    )
+                )
+            )
+          ],
+        )
     );
   }
 }
